76837ae008e5215461c2a76669c3f11a102fb618
Debojit Santra <debojitsantra48@gmail.com>
2025-11-12 12:47:07 +0530

    Simplify install script output messages

--- install.sh
+++ install.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
-echo "    ...............Installing Requirments.........................."
+echo "installing..."
 pkg update -y
 pkg upgrade -y
-echo "    ...............Installing Nmap..................................."
 pkg install nmap -y
-echo "    ...............Installed........................................"
-echo "    .................Updating Termux Packages......................."
 pkg install termux-tools
-echo "     .................'IP Tools by darke'installed on your system....."
 chmod +x itds.sh
-clear
-echo "ITDS IS READY"
-echo "              
-"
-echo "USE: ./itds.sh <options> <website address>"
+echo "installed!!"
